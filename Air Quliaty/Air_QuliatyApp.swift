61dda139f6abb36f4abd1a40b57eb34e4fe23e14
jkwon-dev <kwonejk@gmail.com>
2023-11-14 09:50:57 +0800

    Initial Commit

---
+++ Air Quliaty/Air_QuliatyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Air_QuliatyApp.swift
+//  Air Quliaty
+//
+//  Created by kwon eunji on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Air_QuliatyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
